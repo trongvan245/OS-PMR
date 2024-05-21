8641bf4554ea03bdf0591fde24b84b5c95dc52af
Bui Trong Van <trongvan159357@gmail.com>
2024-05-21 21:47:47 +0700

    update problem(testcase + correct output)

---
+++ Submit/probC_WA.cpp
@@ -0,0 +1,39 @@
+// Hello I'm Nekan
+//
+#include <bits/stdc++.h>
+#define Nekan "test"
+#define fi first
+#define se second
+#define pb push_back
+#define zs(v) ((int)(v).size())
+#define BIT(x, i) (((x) >> (i)) & 1)
+#define pii pair<int, int>
+
+typedef long double ld;
+typedef long long ll;
+
+const int N = 2e5 + 5;
+const long long mod = 1e9 + 7; // 998244353;
+
+using namespace std;
+
+void xuly() {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    if (fopen(Nekan ".inp", "r")) {
+        freopen(Nekan ".inp", "r", stdin);
+        freopen(Nekan ".out", "w", stdout);
+    }
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        xuly();
+}
+
+// Surely nothing could go wrong.
