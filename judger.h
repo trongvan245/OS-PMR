5a0105dc7826649c2021ffe6f75697311aac4caf
Bui Trong Van <trongvan159357@gmail.com>
2024-05-21 16:08:16 +0700

    first judger.cpp

---
+++ judger.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void judge(int problem_id, string dir_code, string &exit_code);
+
+// dir_code: /Submit/probA_Accept.cpp
+// ProbA: probA_AC.cpp, probA_WA.cpp, probA_TLE.cpp
